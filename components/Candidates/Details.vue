ebd92ae4018b2bad38bbb2cc217faa76be78ad81
wasif_alam <wsfalam84@gmail.com>
2023-06-01 18:24:18 +0530

    achieved responsiveness of the candidatedetails drawer

--- components/Candidates/Details.vue
+++ components/Candidates/Details.vue
@@ -18,6 +18,7 @@ import {
   PrinterIcon,
   ChevronDoubleRightIcon,
   TrashIcon,
+  HandThumbUpIcon,
 } from '@heroicons/vue/24/outline';
 
 const { data } = defineProps(['data']);
@@ -36,12 +37,12 @@ watch(data.age, () => {
 <template>
   <div
     id="drawer-right"
-    class="fixed top-0 right-0 z-50 h-screen overflow-y-auto transition-transform translate-x-full bg-white w-[45%]"
+    class="fixed top-0 right-0 z-50 h-screen overflow-y-auto transition-transform translate-x-full bg-white w-[45%] max-2xl:w-1/2 max-xl:w-2/3 max-lg:w-1/2 max-md:w-[55%] max-sm:w-[80%]"
     tabindex="-1"
     aria-labelledby="drawer-right-label"
   >
-    <header class="bg-slate-200 px-5 py-6">
-      <div class="flex justify-between">
+    <header class="bg-slate-200 px-5 py-6 max-xl:p-4">
+      <div class="flex justify-between max-sm:flex-col max-sm:gap-5">
         <div class="flex items-center gap-4">
           <div class="relative">
             <img
@@ -61,42 +62,44 @@ watch(data.age, () => {
 
           <div>
             <div class="flex items-center gap-2">
-              <h3 class="font-medium text-lg">Cody Fisher</h3>
+              <h3 class="font-medium text-lg max-xl:text-base">Cody Fisher</h3>
               <span
-                class="bg-green-100 border rounded-md border-green-400 text-green-600 text-sm px-2"
+                class="bg-green-100 border rounded-md border-green-400 text-green-600 text-sm px-2 max-xl:text-xs"
                 >Active</span
               >
             </div>
 
-            <p class="text-sm text-slate-500 mb-2">Design Team - UI Designer</p>
+            <p class="text-sm text-slate-500 mb-2 max-lg:text-xs">
+              Design Team - UI Designer
+            </p>
             <div class="flex gap-1 items-center">
               <div
-                class="bg-orange-400 w-7 flex items-center justify-center text-sm text-white font-medium rounded-sm`"
+                class="bg-orange-400 w-7 flex items-center justify-center text-sm text-white font-medium rounded-sm max-lg:w-6 max-lg:text-xs"
               >
                 1
               </div>
               <div
-                class="bg-orange-400 w-7 flex items-center justify-center text-sm text-white font-medium rounded-sm`"
+                class="bg-orange-400 w-7 flex items-center justify-center text-sm text-white font-medium rounded-sm max-lg:w-6 max-lg:text-xs"
               >
                 2
               </div>
               <div
-                class="bg-gray-400 w-7 flex items-center justify-center text-sm text-white font-medium rounded-sm`"
+                class="bg-gray-400 w-7 flex items-center justify-center text-sm text-white font-medium rounded-sm max-lg:w-6 max-lg:text-xs"
               >
                 &nbsp;
               </div>
               <div
-                class="bg-gray-400 w-7 flex items-center justify-center text-sm text-white font-medium rounded-sm`"
+                class="bg-gray-400 w-7 flex items-center justify-center text-sm text-white font-medium rounded-sm max-lg:w-6 max-lg:text-xs"
               >
                 &nbsp;
               </div>
               <div
-                class="bg-gray-400 w-7 flex items-center justify-center text-sm text-white font-medium rounded-sm`"
+                class="bg-gray-400 w-7 flex items-center justify-center text-sm text-white font-medium rounded-sm max-lg:w-6 max-lg:text-xs"
               >
                 &nbsp;
               </div>
               <div
-                class="bg-gray-400 w-7 flex items-center justify-center text-sm text-white font-medium rounded-sm`"
+                class="bg-gray-400 w-7 flex items-center justify-center text-sm text-white font-medium rounded-sm max-lg:w-6 max-lg:text-xs"
               >
                 &nbsp;
               </div>
@@ -104,7 +107,7 @@ watch(data.age, () => {
           </div>
         </div>
 
-        <div class="flex gap-6 items-start">
+        <div class="flex gap-6 items-start max-lg:gap-3 max-lg:flex-col">
           <div class="flex items-center gap-3">
             <button class="bg-white p-1.5 shadow-sm rounded-sm">
               <ChevronLeftIcon class="text-slate-500 w-4 h-4" />
@@ -113,18 +116,25 @@ watch(data.age, () => {
               <ChevronRightIcon class="text-slate-500 w-4 h-4" />
             </button>
           </div>
-          <button
-            class="flex gap-1 items-center shadow-sm bg-white p-1.5 rounded-sm"
-          >
-            <ChatBubbleBottomCenterTextIcon class="text-slate-500 w-4 h-4" />
-            <span class="text-xs text-gray-600 font-medium">Send Message</span>
-          </button>
-          <button
-            class="flex gap-1 items-center shadow-sm bg-white p-1.5 rounded-sm"
-          >
-            <EnvelopeIcon class="text-slate-500 w-4 h-4" />
-            <span class="text-xs text-gray-600 font-medium">Send Email</span>
-          </button>
+
+          <div class="flex gap-3">
+            <button
+              class="flex gap-1 items-center shadow-sm bg-white p-1.5 rounded-sm"
+            >
+              <ChatBubbleBottomCenterTextIcon class="text-slate-500 w-4 h-4" />
+              <span class="text-xs text-gray-600 font-medium max-xl:hidden"
+                >Send Message</span
+              >
+            </button>
+            <button
+              class="flex gap-1 items-center shadow-sm bg-white p-1.5 rounded-sm"
+            >
+              <EnvelopeIcon class="text-slate-500 w-4 h-4" />
+              <span class="text-xs text-gray-600 font-medium max-xl:hidden"
+                >Send Email</span
+              >
+            </button>
+          </div>
         </div>
       </div>
     </header>
@@ -138,7 +148,7 @@ watch(data.age, () => {
       >
         <li class="mr-2 ml-6" role="presentation">
           <button
-            class="inline-block p-4 border-b-2 rounded-t-lg uppercase"
+            class="inline-block p-4 border-b-2 rounded-t-lg uppercase max-lg:pl-0"
             id="details-tab"
             data-tabs-target="#details"
             type="button"
@@ -177,7 +187,7 @@ watch(data.age, () => {
         </li>
         <li role="presentation">
           <button
-            class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 uppercase"
+            class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 uppercase max-lg:ml-2"
             id="interviews-tab"
             data-tabs-target="#interviews"
             type="button"
@@ -206,13 +216,15 @@ watch(data.age, () => {
               <h3 class="text-sm font-semibold">Basic Information</h3>
               <div class="flex gap-1 items-center">
                 <PencilSquareIcon class="w-6 h-6 text-slate-700" />
-                <span class="text-sm text-slate-700 font-normal"
+                <span class="text-sm text-slate-700 font-normal max-lg:hidden"
                   >Edit Info</span
                 >
               </div>
             </div>
 
-            <div class="p-4 grid grid-cols-[40fr_60fr] gap-y-4">
+            <div
+              class="p-4 grid grid-cols-[40fr_60fr] gap-y-4 max-lg:grid-cols-1"
+            >
               <div>
                 <span class="text-slate-500 text-sm font-medium">Name</span>
                 <p class="text-sm font-semibold text-gray-700">Cody Fisher</p>
@@ -259,13 +271,15 @@ watch(data.age, () => {
               <h3 class="text-sm font-semibold">Professional Details</h3>
               <div class="flex gap-1 items-center">
                 <PencilSquareIcon class="w-6 h-6 text-slate-700" />
-                <span class="text-sm text-slate-700 font-normal"
+                <span class="text-sm text-slate-700 font-normal max-lg:hidden"
                   >Edit Info</span
                 >
               </div>
             </div>
 
-            <div class="p-4 grid grid-cols-[40fr_60fr] gap-y-4">
+            <div
+              class="p-4 grid grid-cols-[40fr_60fr] gap-y-4 max-lg:grid-cols-1"
+            >
               <div>
                 <span class="text-slate-500 text-sm font-medium"
                   >Current Job Title</span
@@ -357,8 +371,8 @@ watch(data.age, () => {
             <p class="text-sm font-medium">Cody_Fisher_Resume.pdf</p>
           </div>
 
-          <div class="p-4 bg-slate-200">
-            <div class="bg-white shadow-md pt-2 pr-2 pl-6 pb-6">
+          <div class="p-4 bg-slate-200 max-lg:p-2 max-sm:p-1">
+            <div class="bg-white shadow-md pt-2 pr-2 pl-6 pb-6 max-lg:pl-3">
               <header class="flex justify-end mb-2">
                 <div class="flex gap-1 rounded-sm">
                   <div class="border border-slate-400 w-fit p-1">
@@ -493,25 +507,25 @@ watch(data.age, () => {
           </div>
 
           <ol
-            class="flex items-center justify-between w-full space-x-2 text-sm font-medium text-center text-gray-500 bg-white rounded-md sm:space-x-4 border border-slate-200 px-3 py-2"
+            class="flex items-center justify-between flex-wrap w-full text-sm font-medium text-center text-gray-500 bg-white rounded-md gap-4 border border-slate-200 px-3 py-2 max-lg:justify-normal"
           >
-            <li class="flex items-center text-gray-600">
+            <li class="flex items-center text-gray-600 max-lg:p-[.1rem]">
               <span>Screening</span>
               <ChevronDoubleRightIcon class="w-4 h-4 ml-2 sm:ml-4" />
             </li>
-            <li class="flex items-center">
+            <li class="flex items-center max-lg:p-[.1rem]">
               <span class="">Design Challange</span>
               <ChevronDoubleRightIcon class="w-4 h-4 ml-2 sm:ml-4" />
             </li>
-            <li class="flex items-center text-orange-400">
+            <li class="flex items-center text-orange-400 max-lg:p-[.1rem]">
               <span>Interview</span>
               <ChevronDoubleRightIcon class="w-4 h-4 ml-2 sm:ml-4" />
             </li>
-            <li class="flex items-center">
+            <li class="flex items-center max-lg:p-[.1rem]">
               <span>Test</span>
               <ChevronDoubleRightIcon class="w-4 h-4 ml-2 sm:ml-4" />
             </li>
-            <li class="flex items-center">
+            <li class="flex items-center max-lg:p-[.1rem]">
               <span>Hired</span>
               <ChevronDoubleRightIcon class="w-4 h-4 ml-2 sm:ml-4" />
             </li>
@@ -522,7 +536,9 @@ watch(data.age, () => {
               <h3 class="text-sm font-semibold">Detail</h3>
             </div>
 
-            <div class="p-4 grid grid-cols-[40fr_60fr] gap-y-4">
+            <div
+              class="p-4 grid grid-cols-[40fr_60fr] gap-y-4 max-lg:grid-cols-1"
+            >
               <div>
                 <span class="text-slate-500 text-sm font-medium"
                   >Current Status</span
@@ -541,9 +557,11 @@ watch(data.age, () => {
               </div>
               <div>
                 <span class="text-slate-500 text-sm font-medium">Assignee</span>
-                <div class="flex gap-2 items-center">
+                <div
+                  class="flex gap-2 items-center max-lg:flex-col max-lg:items-start max-lg:gap-1 max-lg:mt-1"
+                >
                   <img
-                    src="@/assets/images/pp-2.jpg"
+                    src="@/assets/images/pp-2.jpg "
                     alt="owner-image"
                     class="w-7 h-7 rounded-full"
                   />
@@ -559,7 +577,9 @@ watch(data.age, () => {
               </div>
               <div>
                 <span class="text-slate-500 text-sm font-medium">Owner</span>
-                <div class="flex gap-2 items-center">
+                <div
+                  class="flex gap-2 items-center max-lg:flex-col max-lg:items-start max-lg:gap-1 max-lg:mt-1"
+                >
                   <img
                     src="@/assets/images/pp-3.jpg"
                     alt="owner-image"
@@ -702,10 +722,14 @@ watch(data.age, () => {
               </header>
 
               <div
-                class="border border-slate-200 rounded-md px-3 py-4 border-l-4 border-l-cyan-500"
+                class="border border-slate-200 rounded-md px-3 py-4 border-l-4 border-l-cyan-500 shadow-sm"
               >
-                <div class="items-center grid grid-cols-4">
-                  <div class="flex items-center gap-2">
+                <div
+                  class="items-center grid grid-cols-[30fr_20fr_30fr_20fr] gap-x-2 max-xl:grid-cols-[35fr_20fr_30fr_15fr] max-lg:grid-cols-1 max-lg:gap-y-4"
+                >
+                  <div
+                    class="flex items-center gap-2 max-lg:flex-col max-lg:items-start"
+                  >
                     <img
                       class="w-8 h-8 rounded-full"
                       src="@/assets/images/pp-1.jpg"
@@ -733,9 +757,12 @@ watch(data.age, () => {
                   </div>
 
                   <button
-                    class="text-green-700 text-xs font-semibold border border-green-700 px-3 py-2 rounded-md w-fit justify-self-end"
+                    class="text-green-700 text-xs font-semibold border border-green-700 px-3 py-2 rounded-md w-fit justify-self-end max-xl:p-2"
                   >
-                    Submit Feedback
+                    <span class="max-xl:hidden"> Submit Feedback </span>
+                    <HandThumbUpIcon
+                      class="w-5 h-5 hidden max-xl:inline-block"
+                    />
                   </button>
                 </div>
               </div>
@@ -757,10 +784,14 @@ watch(data.age, () => {
               </header>
 
               <div
-                class="border border-slate-200 rounded-md px-3 py-4 border-l-4 border-l-yellow-400"
+                class="border border-slate-200 rounded-md px-3 py-4 border-l-4 border-l-yellow-400 shadow-sm"
               >
-                <div class="items-center grid grid-cols-4">
-                  <div class="flex items-center gap-2">
+                <div
+                  class="items-center grid grid-cols-[30fr_20fr_30fr_20fr] gap-x-2 max-xl:grid-cols-[35fr_20fr_30fr_15fr] max-lg:grid-cols-1 max-lg:gap-y-4"
+                >
+                  <div
+                    class="flex items-center gap-2 max-lg:flex-col max-lg:items-start"
+                  >
                     <img
                       class="w-8 h-8 rounded-full"
                       src="@/assets/images/pp-2.jpg"
@@ -788,9 +819,12 @@ watch(data.age, () => {
                   </div>
 
                   <button
-                    class="text-green-700 text-xs font-semibold border border-green-700 px-3 py-2 rounded-md w-fit justify-self-end"
+                    class="text-green-700 text-xs font-semibold border border-green-700 px-3 py-2 rounded-md w-fit justify-self-end max-xl:p-2"
                   >
-                    Submit Feedback
+                    <span class="max-xl:hidden"> Submit Feedback </span>
+                    <HandThumbUpIcon
+                      class="w-5 h-5 hidden max-xl:inline-block"
+                    />
                   </button>
                 </div>
               </div>
@@ -812,10 +846,14 @@ watch(data.age, () => {
               </header>
 
               <div
-                class="border border-slate-200 rounded-md px-3 py-4 border-l-4 border-l-cyan-500"
+                class="border border-slate-200 rounded-md px-3 py-4 border-l-4 border-l-cyan-500 shadow-sm"
               >
-                <div class="items-center grid grid-cols-4">
-                  <div class="flex items-center gap-2">
+                <div
+                  class="items-center grid grid-cols-[30fr_20fr_30fr_20fr] gap-x-2 max-xl:grid-cols-[35fr_20fr_30fr_15fr] max-lg:grid-cols-1 max-lg:gap-y-4"
+                >
+                  <div
+                    class="flex items-center gap-2 max-lg:flex-col max-lg:items-start"
+                  >
                     <img
                       class="w-8 h-8 rounded-full"
                       src="@/assets/images/pp-3.jpg"
@@ -843,9 +881,12 @@ watch(data.age, () => {
                   </div>
 
                   <button
-                    class="text-green-700 text-xs font-semibold border border-green-700 px-3 py-2 rounded-md w-fit justify-self-end"
+                    class="text-green-700 text-xs font-semibold border border-green-700 px-3 py-2 rounded-md w-fit justify-self-end max-xl:p-2"
                   >
-                    Submit Feedback
+                    <span class="max-xl:hidden"> Submit Feedback </span>
+                    <HandThumbUpIcon
+                      class="w-5 h-5 hidden max-xl:inline-block"
+                    />
                   </button>
                 </div>
               </div>
